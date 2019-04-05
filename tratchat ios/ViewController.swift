ba2e5d5a50a9e8ac5cff7e1bb72e65c8cbe0de18
Ryan Trattner <ryantrattner@gmail.com>
2019-04-04 22:43:57 -0700

    Initial Commit

---
+++ tratchat ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tratchat ios
+//
+//  Created by Ryan Trattner on 4/4/19.
+//  Copyright © 2019 Ryan Trattner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
